284e27867c4ebc5748bb91350020fbd646be7f02
PMS22 <prathams99@rediff.com>
2018-09-16 01:25:44 +0530

    SystemUI: Add Gaming mode tile
    Enabling the tile will :-
    - Show a "First-click" informative dialog
    - Change ringer mode to vibrate
    - Increase the media volume to max
    - Disable Heads Up
    - Turn off Hardware keys
    
    - Turning off the tile will change all the values back to default
    - I haven't enabled Expanded desktop as I dont think any game runs
      in non expanded mode, so better to keep it clean.
    - Duplicate the SystemSetting class from SecureSetting one grabbing
      the System Settings string :-
      https://github.com/FireHound/android_frameworks_base/blob/o8.1/packages/SystemUI/src/com/android/systemui/qs/SecureSetting.java
    
    Change-Id: I15d55eef58d24ac862a6f002bf2a77299037e41c

--- packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
+++ packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
@@ -32,6 +32,7 @@ import com.android.systemui.qs.tiles.ColorInversionTile;
 import com.android.systemui.qs.tiles.DataSaverTile;
 import com.android.systemui.qs.tiles.DndTile;
 import com.android.systemui.qs.tiles.FlashlightTile;
+import com.android.systemui.qs.tiles.GamingModeTile;
 import com.android.systemui.qs.tiles.HotspotTile;
 import com.android.systemui.qs.tiles.IntentTile;
 import com.android.systemui.qs.tiles.LocationTile;
@@ -98,6 +99,8 @@ public class QSFactoryImpl implements QSFactory {
                 return new NightDisplayTile(mHost);
             case "nfc":
                 return new NfcTile(mHost);
+            case "gaming":
+                return new GamingModeTile(mHost);
         }
 
         // Intent tiles.
